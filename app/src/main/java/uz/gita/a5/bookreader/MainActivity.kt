d18cb33e96de9131d1be8bd8fb0756ad969d4ec6
Muhammadali <muhammadalinomonov837@gmail.com>
2023-05-22 23:37:19 +0500

    git

--- app/src/main/java/uz/gita/a5/bookreader/MainActivity.kt
+++ app/src/main/java/uz/gita/a5/bookreader/MainActivity.kt
@@ -5,11 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import by.kirich1409.viewbindingdelegate.viewBinding
 import uz.gita.a5.bookreader.databinding.ActivityMainBinding
-
-
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
